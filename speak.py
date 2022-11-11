81d2741c0ffd422ba771b1bef5cc60337307e333
Harish-Boopathi <109892627+Harish-Boopathi@users.noreply.github.com>
2022-11-11 11:55:12 +0530

    Create speak.py

---
+++ speak.py
@@ -0,0 +1,11 @@
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voices', voices[2].id)
+engine.setProperty('rate',168)
+engine.setProperty('volume', 2.0)
+engine.runAndWait()
+
+       
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
