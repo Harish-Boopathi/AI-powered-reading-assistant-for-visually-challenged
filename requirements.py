f0ca93cf1239c9f791c154fec165635e0a3c2912
Harish-Boopathi <109892627+Harish-Boopathi@users.noreply.github.com>
2022-11-11 10:54:33 +0530

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,18 @@
+import pygame
+import speech_recognition as sr   #pip install speechRecognition
+from pygame import mixer
+import pyttsx3      #pip install pyttsx3
+import datetime
+import wikipedia    #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+import cv2
+import pytesseract    # will convert the image to text string
+import gtts  
+from playsound import playsound
+from PIL import Image
+from googletrans import Translator
+from gtts import gTTS      #Import Google Text to Speech
+from IPython.display import Audio 
+import RPi.GPIO as GPIO
