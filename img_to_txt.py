82eafdc1d4545c7f500a2b51ed9aa851da52d4ce
Harish-Boopathi <109892627+Harish-Boopathi@users.noreply.github.com>
2022-12-23 19:10:14 +0530

    Text extraction

--- img_to_txt.py
+++ img_to_txt.py
@@ -6,6 +6,7 @@ print(img)
 # converts the image to result and saves it into result variable
 result = pytesseract.image_to_string(img)
 
+
 # write text in a text file and save it to source path
 with open('your_path/output.txt',mode ='w') as file:
     file.write(result)
