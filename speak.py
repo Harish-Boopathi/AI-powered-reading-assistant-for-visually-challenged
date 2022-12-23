94df9560999be6ff274f5a04c4efbb73ef7ff1cd
Harish-Boopathi <109892627+Harish-Boopathi@users.noreply.github.com>
2022-12-23 19:19:12 +0530

    giving voice output

--- speak.py
+++ speak.py
@@ -4,8 +4,7 @@ engine.setProperty('voices', voices[2].id)
 engine.setProperty('rate',168)
 engine.setProperty('volume', 2.0)
 engine.runAndWait()
-
-       
+    
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
